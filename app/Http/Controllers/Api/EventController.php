08c0e636b882cae4170d1d6223bbc0c61ec477ab
Yin Nyein Aye <yinnyeinaye.yinnyein@gmail.com>
2023-09-14 14:20:59 +0630

    Adding Event & Attendees CRUD

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
+use App\Models\Event;
+use App\Models\User;
+use App\Models\Attendee;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceResponse;
+use Illuminate\Support\Facades\Gate;
+
+class EventController extends Controller
+{
+    use CanLoadRelationships;
+    private array $relations = ['user','attendees','attendees.user'];
+
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function __construct()
+    {
+        $this->middleware("auth:sanctum")->except(["index","show"]);
+        $this->middleware("throttle:60,1")
+            ->only(["store","update","destroy"]);
+        $this->authorizeResource(Event::class,"event");
+    }
+
+    public function index()
+    {
+        $query = $this->loadRelationships(Event::query());
+        return EventResource::collection($query->latest()->paginate(3));
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time'
+            ]),
+            'user_id' => $request->user()->id
+        ]);
+        return new EventResource($this->loadRelationships($event));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event){
+
+        return new EventResource($this->loadRelationships($event));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Event $event)
+    {
+        // if(Gate::denies("update-event",$event)){
+        //     abort(403,"You are not authorized to update this event");
+        // }
+
+        // $this->authorize('update-event',$event);
+        $event->update($request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'sometimes|date',
+            'end_time' => 'sometimes|date|after:start_time'
+        ]));
+        return new EventResource($this->loadRelationships($event));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event)
+    {
+        $event->delete();
+        return response(status: 204);
+        // return response()->json([
+        //     "message" => "Event is successfully deleted"
+        // ]);
+    }
+}
---
+++ app/Http/Traits/CanLoadRelationships.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+trait CanLoadRelationships
+{
+    public function loadRelationships(Model|EloquentBuilder|QueryBuilder|HasMany $for,
+    ?array $relations = null):Model|EloquentBuilder|QueryBuilder|HasMany
+    {
+        $relations = $relations ?? $this->relations ?? [];
+        // dd($for instanceof Model);
+        foreach($relations as $relation){
+            $for->when($this->shouldIncludeRelation($relation),
+            fn($q) => $for instanceof Model ? $for->load($relation) : $q->with($relation)
+            );
+        }
+        // dd($for);
+        return $for;
+    }
+
+    protected function shouldIncludeRelation(string $relation): bool
+    {
+        $include = request()->query('include');
+        if(!$include){
+            return false;
+        }
+
+        $relations = array_map("trim",explode(",",$include));
+        return in_array($relation,$relations);
+    }
+}
